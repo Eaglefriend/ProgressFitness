29f767b14a748f95e302eab3386e39715386a742
Adrian Mosquera Rodriguez <adrian.mosquera@protonmail.com>
2019-05-21 02:19:53 +0200

    Manifest Datei wurde hinzugefügt

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,7 @@
-console.log('Jello');
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(function() {
+        console.log('Service worker registered!');
+      });
+  }
