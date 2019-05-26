735111c5dc7e0688a984898d5ac9922054079e64
Adrian Mosquera Rodriguez <adrian.mosquera@protonmail.com>
2019-05-26 22:21:54 +0200

    challenges grid changed

--- public/js/app.js
+++ public/js/app.js
@@ -11,9 +11,9 @@ window.addEventListener('load', async e =>{
 });
 
 //prevent Add to Homescreen Install Banner Default to show at later point in time
-window.addEventListener('beforeinstallprompt', function(event){
+/*window.addEventListener('beforeinstallprompt', function(event){
     console.log('beforeinstallprompt fired');
     event.preventDefault();
     defferedPrompt = event; //store event in variable
     return false; //dont do anything upon this event
-});
+});*/
