36eab6160ab4e6c44dfad492183a6a718220ddbd
Adrian Mosquera Rodriguez <adrian.mosquera@protonmail.com>
2019-05-31 16:12:28 +0200

    Caching wih Service Worker

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,15 @@
 var defferedPrompt;
 
-window.addEventListener('load', async e =>{
-    if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () =>{
         navigator.serviceWorker
           .register('/sw.js')
-          .then(function() {
-            console.log('Service worker registered!');
-          });
-      }
-});
+          .then(reg => {
+            console.log('Service worker: Registered!');
+          })
+          .catch(err => console.log(`Service Worker: Error: ${err}`))
+      });
+};
 
 //prevent Add to Homescreen Install Banner Default to show at later point in time
 /*window.addEventListener('beforeinstallprompt', function(event){
@@ -16,4 +17,70 @@ window.addEventListener('load', async e =>{
     event.preventDefault();
     defferedPrompt = event; //store event in variable
     return false; //dont do anything upon this event
-});*/
+});
+*/
+/*
+var promise = new Promise(function(resolve, reject) {
+  setTimeout(function(){
+    //resolve('This is executed once the timer is done!');
+    reject({code: 500, message: 'An error has ocurred!'});
+  },3000);
+});
+
+*/
+//Fetch API
+
+//GET-Request
+fetch('https://httpbin.org/ip')
+  .then(function(response){
+    console.log(response);
+    return response.json();
+  })
+  .then(function(data){
+    console.log(data);
+  })
+  .catch(function(err){
+    console.log('Fetch error: ' + err);
+  });
+
+  //POST-Request
+  fetch('https://httpbin.org/post', {
+    //fetch Request
+    method: 'POST',//Post request
+    headers: {
+      'Content-Type': 'application/json',//what kind of data we are sending
+      'Accept': 'application/json'//Accept to get back Json-data
+    },
+    mode: 'cors',
+    body: JSON.stringify({/*convert Javascript-Object into JSON-data*/ message: 'Does this work?'})//Data we want to send
+  })
+  .then(function(response){
+    console.log(response);
+    return response.json();//Extract data from Response
+  })
+  .then(function(data){
+    console.log(data);//Output Data in log
+  })
+  .catch(function(err){
+    console.log('Fetch error: ' + err);
+  });
+/*
+promise.then(function(text){
+  return text;
+}, function(err){
+  console.log(err.code, err.message)
+}).then(function(newText){ 
+  console.log("new text");
+});
+*/
+/*
+promise.then(function(text){
+  return text;
+}).then(function(newText){
+    console.log(newText);
+}).catch(function(err){
+    console.log(err.code, err.message);
+});
+*/
+
+/*console.log('This is executed right after setTimeout()');*/
--- public/js/homeButton.js
+++ public/js/homeButton.js
@@ -1,4 +1,4 @@
-let athsb = document.querySelector('#athsb');
+/*let athsb = document.querySelector('#athsb');
 
 
 const openAddToHomescreenBanner =()=>{
@@ -23,3 +23,34 @@ const openAddToHomescreenBanner =()=>{
 }
 
 athsb.addEventListener('click', openAddToHomescreenBanner);
+*/
+
+
+let deferredPrompt;
+let btnAdd = document.querySelector('#btnAdd');
+
+window.addEventListener('beforeInstallPrompt', e=>{
+    e.preventDefault();
+
+    defferedPrompt = e;
+
+    btnAdd.style.display = 'block';
+});
+
+
+btnAdd.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    btnAdd.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
