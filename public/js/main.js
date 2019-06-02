23a5ac1abf7c3d465e835c29f12abca192c54012
Adrian Mosquera Rodriguez <adrian.mosquera@protonmail.com>
2019-06-02 21:02:54 +0200

    challenges-main-page designed

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+/*let ch_names_end = document.getElementById('ch_names_endurance');
+let ch_names_str = document.getElementById('ch_names_strenght');
+let ch_names_mass = document.getElementById('ch_names_mass');
+
+let ch_img_end = document.getElementById('ch_img_endurance');
+let ch_img_str = document.getElementById('ch_img_strength');
+let ch_img_mass = document.getElementById('ch_img_mass');
+
+ch_names_end.hover(()=>{
+    this.CSS({'cursor':'pointer'});
+})
+
+ch_names_end.addEventListener(onmouseover, ()=>{
+    ch_names_end.style.backgroundColor = '#F6863B';
+});
+
+*/
