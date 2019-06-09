c14b14881655f942ab750d1daaa80c4dd2807b12
Adrian Mosquera Rodriguez <adrian.mosquera@protonmail.com>
2019-06-09 23:00:28 +0200

    riesen Commit mit dem Desktop-Layout +Animation für menübutton

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,70 @@
-/*let ch_names_end = document.getElementById('ch_names_endurance');
-let ch_names_str = document.getElementById('ch_names_strenght');
-let ch_names_mass = document.getElementById('ch_names_mass');
+//Desktop Navigation
+//let navBtn = document.querySelector('.menuBtn_dsk');
+let openNav = document.querySelector('.openNavBtn');
 
-let ch_img_end = document.getElementById('ch_img_endurance');
-let ch_img_str = document.getElementById('ch_img_strength');
-let ch_img_mass = document.getElementById('ch_img_mass');
+let lineOne = document.querySelector('.line_one');
+let lineTwo = document.querySelector('.line_two');
+let lineThree = document.querySelector('.line_three');
+let lines = [lineOne, lineTwo, lineThree];
 
-ch_names_end.hover(()=>{
-    this.CSS({'cursor':'pointer'});
-})
+let closeNav = document.getElementById('closeNavBtn');
+let sideMenu = document.getElementById('sideMenu');
+sideMenu.style.display='none';
 
-ch_names_end.addEventListener(onmouseover, ()=>{
-    ch_names_end.style.backgroundColor = '#F6863B';
+
+/*openNav.addEventListener('click',()=>{
+   
+    
+    closeNav.style.display='block';
+    openNav.style.display='none';
+});
+*/
+
+//Menu-Button Hover
+const tlm = new TimelineMax({});
+
+openNav.addEventListener('mouseenter',()=>{
+
+    if(openNav.classList.contains('js-x')){
+      return;   
+    }
+    tlm.staggerTo(lines, 0.25, {transformOrigin: "50% 50%", scaleX: 1.5, repeat: 1, yoyo: true, ease: Power2.easeIn}, 0.125);
+    
 });
 
+
+
+//Menu-Button Click
+const toggleMenu = new TimelineMax({paused: true, reversed: true});
+
+toggleMenu
+    .to(lineTwo, 0.125, {opacity: 0})
+    .to(lineOne, 0.25, {transformOrigin: "50% 50%", y:10, ease: Power2.easeInOut}, "slide")
+    .to(lineThree, 0.25, {transformOrigin: "50% 50%", y:-10, ease: Power2.easeInOut}, "slide")
+    .to(openNav, 0.5, {transformOrigin: "50% 50%", rotation: 360, ease: Power4.easeInOut})
+    .to(lineOne, 0.25, {rotation: 45, ease: Power2.easeInOut},"cross")
+    .to(lineThree, 0.25, {rotation: -45, ease: Power2.easeInOut},"cross");
+    
+openNav.addEventListener('click', ()=>{
+    openNav.classList.toggle('js-x');
+    toggleMenu.reversed() ? toggleMenu.play() : toggleMenu.reverse();
+    
+
+    if(sideMenu.style.display==='none'){
+        sideMenu.style.display='flex';
+    }else{
+        sideMenu.style.display='none';
+    }
+})
+
+
+/*
+closeNav.addEventListener('click',()=>{
+
+    sideMenu.style.display='none';
+    closeNav.style.display='none';
+    openNav.style.display='block';
+    
+})
 */
+
