635b558b226e8fe958db648619090ab6b8faab1c
Adrian Mosquera Rodriguez <adrian.mosquera@protonmail.com>
2019-06-11 04:15:39 +0200

    new service worker with offline capability and fallback html

--- public/sw.js
+++ public/sw.js
@@ -68,14 +68,86 @@ self.addEventListener('fetch', e=>{
 
 */
 
+const staticCacheName = 'site-static-v1';
+const dynamicCacheName = 'site-dynamic-v1';
+const assets = [    //store Request Urls -> later store as value for Assets the Response
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/main.js',
+    '/js/homebutton.js',
+    '/media/img/challenges_img/endurance_challenge.jpg',
+    '/media/img/challenges_img/strength_challenge.jpg',
+    '/media/img/challenges_img/mass_challenge.jpg',
+    '/media/img/icons/',
+    '/scss/main.css',
+    'https://fonts.googleapis.com/css?family=Lato:300,400,400i,700,700i,900i&display=swap',
+    'https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js',
+    'https://fonts.gstatic.com/s/lato/v15/S6u_w4BMUTPHjxsI5wq_Gwft.woff2',
+    'https://fonts.gstatic.com/s/lato/v15/S6u_w4BMUTPHjxsI3wi_Gwft.woff2',
+    'https://fonts.gstatic.com/s/lato/v15/S6uyw4BMUTPHjx4wXg.woff2',
+    'https://fonts.gstatic.com/s/lato/v15/S6u9w4BMUTPHh7USSwiPGQ.woff2',
+    'favicon.ico',
+    '/pages/fallback.html'
+
+];
+
+
+// Cache Size Limit function
+const limitCacheSize = (name, size)=>{
+    caches.open(name).then(cache =>{
+        cache.keys().then(keys =>{
+            if(keys.length > size){
+                cache.delete(keys[0])//delete oldest item
+                .then(limitCacheSize(name,size));//recall function until length of keys !> size
+            }
+        })
+    });
+};
+
+
 self.addEventListener('install', evt =>{
-    console.log('Service Worker installed!');
+    //console.log('Service Worker installed!');
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache=>{//open static caches
+            console.log('Caching shell assets!');
+            cache.addAll(assets);
+        })
+    );
+    
 });
 
 self.addEventListener('activate', evt =>{
-    console.log('Service Worker activated!');
+    //console.log('Service Worker activated!');
+    evt.waitUntil(
+        caches.keys().then(keys =>{ //go to all chaches and get keys
+            //console.log(keys);
+            return Promise.all(keys         //take Array of Promises with all keys and filter array
+                .filter(key => key!== staticCacheName && key!==dynamicCacheName)//if names are not equal, it stays in Array
+                .map(key => caches.delete(key))    //Map the key of leftover Array to Array of Promises and delete their cache
+            )
+        })
+    );
 });
 
 self.addEventListener('fetch', evt =>{
     console.log('fetch event:', evt);
+
+    evt.respondWith(//pause fetch event and instead respond with
+        caches.match(evt.request)//is sth in cache that matches request?
+            .then(cacheRes =>{
+                return cacheRes || fetch(evt.request).then(fetchRes =>{ //if its not in the cachem, carry on initial Fetch Request to Server
+                    return caches.open(dynamicCacheName).then(cache =>{//open Dynamic Cache
+                        cache.put(evt.request.url, fetchRes.clone());//put clone of Response in dyn cache
+                        limitCacheSize(dynamicCacheName, 15);//is cache over certain size? Here 15 items before deleting old cache items
+                        return fetchRes;
+                    })
+                });
+            })
+            .catch(()=> {//Fallback if Page is not in Cache nor can be fetched from server because offline
+                if(evt.request.url.indexOf('.html')>-1){//is requested offline stuff an html page?
+                    return caches.match('pages/fallback.html');
+                }
+        })
+    );
 });
